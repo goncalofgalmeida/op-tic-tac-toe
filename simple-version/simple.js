25d1a3b52267ff877b41243932af86b7df3b0858
g24force <user.email>
2025-02-12 14:33:14 +0000

    Simplify creation of the board variable
    -Game is always fixed size (3x3)
    -No need to create other variables and loops
    -More efficient approach

--- simple-version/simple.js
+++ simple-version/simple.js
@@ -1,15 +1,10 @@
 // Keeps state of the board
 function Gameboard() {
-	const rows = 3;
-	const columns = 3;
-	const board = [];
-
-	for (let i = 0; i < rows; i++) {
-		board[i] = [];
-		for (let j = 0; j < columns; j++) { 
-			board[i].push(Cell());
-		}
-	}
+	const board = [
+		[Cell(), Cell(), Cell()],
+		[Cell(), Cell(), Cell()],
+		[Cell(), Cell(), Cell()]
+	];
 
 	const getBoard = () => board;
 
